06456130e7bbf56f26275ca8a52a4d9facc7d8e6
Administrator <Administrator@PC-20170630VXGY>
2019-04-18 23:10:52 +0800

    test push

--- ssmdemo/src/main/java/com/sve/service/UserServiceImpl.java
+++ ssmdemo/src/main/java/com/sve/service/UserServiceImpl.java
@@ -14,27 +14,21 @@ public class UserServiceImpl implements UserService {
 	
 	@Autowired
 	private UserDao userDao;
-	@Override
 	public List<User> getAllUser(int begin, int num) {
 		return userDao.getAllUser(begin, num);
 	}
-	@Override
 	public User getById(int id) {
 		return userDao.getById(id);
 	}
-	@Override
 	public int addUser(User user) {
 		return userDao.addUser(user);
 	}
-	@Override
 	public int updateUser(User user) {
 		return userDao.updateUser(user);
 	}
-	@Override
 	public int deleteUser(int id) {
 		return userDao.deleteUser(id);
 	}
-	@Override
 	public int getUserCount() {
 		return userDao.getUserCount();
 	}
